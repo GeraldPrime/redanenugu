808f2b9a3247c21fd84f14fc2678ae990f81c138
GeraldPrime <geraldokeke68@gmail.com>
2025-07-05 14:14:14 +0100

    static fix

--- redanenugu/settings.py
+++ redanenugu/settings.py
@@ -123,7 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+
+# Add this to your settings.py
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Static Files Configuration
 STATIC_URL = '/static/'
